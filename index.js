4eaa25495d01e621721944ad26706d9b95542bc6
sjpatel <sjpatel298@gmail.com>
2023-07-22 10:27:01 +0530

    DIPLOY
    DEPLOY MERN

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+var cors = require('cors');
+
+const path = require('path')
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+require('./db/conn');
+
+ 
+const app = express();
+const port = process.env.PORT  || 5000;
+app.use(cors());
+
+
+app.use(express.json());
+app.use("/imagesvedio",express.static("imagesvedio"));
+
+
+
+app.use(require('./Client/router/Autentication'));
+app.use(require('./Admin/Router/TopicsStore'));
+app.use(require('./Client/router/GetContent'));
+app.use(require('./Client/router/QusAns'));
+
+app.use(express.static(path.join(__dirname, '/RYSCODE/build')))
+app.get('*',(res,req)=>{
+    res.sendFile(path.join(__dirname, '/RYSCODE/build/index.html'));
+})
+
+app.listen(port, () => {
+    console.log(`Connection successfully on port ${port} `);
+})
